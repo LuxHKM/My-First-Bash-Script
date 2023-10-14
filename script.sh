e913ae889155467cb8213619471d5d30ca301b70
LuxCodeDev <sluxofficial6@gmail.com>
2023-10-14 18:41:29 +0200

    Add Colours

--- script.sh
+++ script.sh
@@ -1,2 +1,15 @@
-# BY LUXCODE
-echo "This is your Private IP: $(hostname -I | awk '{print $1}')"
+#!/bin/bash
+
+# Author @LuxCodeDev
+
+#Colours
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
+echo -e "\n${yellowColour} [+]${endColour} This is your Private IP: ${blueColour}$(hostname -I | awk '{print $1}')${endColour}"
